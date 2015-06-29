dbd5e20d6a88f46e29e49ccaac37a8603da5484e
Christopher Boutillé <christopher.boutille@me.com>
2015-06-29 09:20:15 +0200

    Add complete

---
+++ site/blueprints/complete.php
@@ -0,0 +1,17 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Complete
+pages: false
+files: false
+deletable: false
+fields:
+  title:
+    label: Page
+    type:  text
+  subtitle:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
+    size:  large
