0be22af5ab503eef3de99f1d576a215b034228e8
Christopher Boutillé <christopher.boutille@me.com>
2016-10-01 18:50:11 +0200

    Petite mise à jour. 👍🏻

--- site/templates/products.php
+++ site/templates/products.php
@@ -14,24 +14,24 @@
   <ul class="teaser cf">
     <?php foreach($page->children()->visible()->flip() as $product): ?>
     <li>
-      <h3><a href="<?php echo $product->url() ?>"><?php echo $product->title()->html() ?></a></h3>
+      <h4><a href="<?php echo $product->url() ?>"><?php echo $product->title()->html() ?></a></h4>
       <?php if($image = $product->images()->sortBy('sort', 'asc')->first()): ?>
       <?php echo thumb($image, array('width' => 300, 'height' => 300, 'crop' => true)); ?>
       <?php endif ?>
-      <?php if($product->soldout() != ''): ?>
-      <h4 class="cf">
-        <button class="btn-disabled" type="submit" disabled="">Sold Out</button>
-        <?php echo $site->currency_symbol() ?><?php echo $product->price() ?>
-      </h4>
-      <?php else: ?>
+      <?php if($product->soldout() != 'true'): ?>
       <form method="post" action="<?php echo url('cart') ?>">
         <input type="hidden" name="action" value="add">
         <input type="hidden" name="id" value="<?php echo $product->uid() ?>">
-        <h4 class="cf">
+        <h4 class="prix cf">
           <button class="btn" type="submit">Add to cart</button>
           <?php echo $site->currency_symbol() ?><?php echo $product->price() ?>
         </h4>
       </form>
+      <?php else: ?>
+      <h4 class="prix cf">
+        <button class="btn-disabled" type="submit" disabled="">Sold Out</button>
+        <?php echo $site->currency_symbol() ?><?php echo $product->price() ?>
+      </h4>
       <?php endif ?>
     </li>
     <?php endforeach ?>
